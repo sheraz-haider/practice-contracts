604f952ce9d7c51d2818a31952263986d1864c5d
Sheraz Haider <sheraz5006@gmail.com>
2021-12-26 11:04:35 +0500

    some setup and change in config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-watcher');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -17,5 +20,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.5',
+  // defaultNetwork: 'localhost',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_NODE,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  watcher: {
+    compile: {
+      tasks: ['compile'],
+      files: ['./contracts'],
+      verbose: true,
+    },
+  },
 };
