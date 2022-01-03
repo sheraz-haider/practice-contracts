8db4eda573aa5d73aa21989230a561604141fa37
Sheraz Haider <sheraz5006@gmail.com>
2022-01-03 16:03:59 +0500

    fixes in game contract

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 contract Game {
-  uint contractCtreationTime;
   uint chngPlyrLvlWaitTime = 0;
   uint public playerCount = 0;
 
@@ -12,24 +11,25 @@ contract Game {
     uint id;
     string name;
     Level playerLevel;
+    uint createdTime;
   }
 
   mapping(address => Player) public players;
 
   constructor(uint _chngPlyrLvlWaitTime) {
     chngPlyrLvlWaitTime = _chngPlyrLvlWaitTime;
-    contractCtreationTime = block.timestamp;
   }
 
   function addPlayer(string memory name) public {
     playerCount += 1;
-    players[msg.sender] = Player(playerCount, name, Level.Beginner);
+    players[msg.sender] = Player(playerCount, name, Level.Beginner, block.timestamp);
   }
 
   function changePlayerLevel(address playerAddress, uint8 level) public {
-    require(block.timestamp >= contractCtreationTime + chngPlyrLvlWaitTime, "Please wait for sometime to change player level");
+    Player storage player = players[playerAddress];
+    require(block.timestamp >= player.createdTime + chngPlyrLvlWaitTime, "Please wait for sometime to change player level");
     require(level <= uint8(Level.Advance), "Level must be from 0 to 2");
-    players[playerAddress].playerLevel = Level(level);
+    player.playerLevel = Level(level);
   }
 
   function getPlayerLevel(address playerAddress) public view returns(Level) {
